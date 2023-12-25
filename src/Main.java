24221b2e3a0e8f6b9b88dc04f933b4709091ac70
İmranov <semtexpert@gmail.com>
2023-12-25 15:48:38 +0300

    Vesiyon 1.0

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int n;
+        int r;
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("n'i girin: ");
+        n = scanner.nextInt();
+
+        System.out.print("r'yi girin: ");
+        r = scanner.nextInt();
+
+        if (r > n) {
+            System.out.println("Hata: r, n'den büyük olamaz!");
+        } else {
+            long kombinasyon = 1;
+
+            for (int i = 1; i <= r; i++) {
+                kombinasyon *= n;
+                kombinasyon /= i;
+                n--;
+            }
+
+            System.out.println("C(" + n + ", " + r + ") = " + kombinasyon);
+        }
+
     }
 }
